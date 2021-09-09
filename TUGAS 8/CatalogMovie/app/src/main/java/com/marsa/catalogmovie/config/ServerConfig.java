5902c4028d83db748bd589948ba56684c2c8ab3f
MarsaHawari <68333057+MarsaHawari@users.noreply.github.com>
2021-09-09 23:37:03 +0700

    Tugas 8

---
+++ TUGAS 8/CatalogMovie/app/src/main/java/com/marsa/catalogmovie/config/ServerConfig.java
@@ -0,0 +1,7 @@
+package com.marsa.catalogmovie.config;
+
+public class ServerConfig {
+    public  static final String URL_BASE = "https://www.themoviedb.org/";
+    public static final String API_ENDPOINT = URL_BASE;
+}
+
